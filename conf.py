bc9079c74473de834ce398a5d869f0a7ee8cdb54
Dominik Stańczak <stanczakdominik@gmail.com>
2020-02-14 14:18:33 +0100

    GNU Terry Pratchett

--- conf.py
+++ conf.py
@@ -142,8 +142,6 @@ NAVIGATION_LINKS = {
             (
                 ("/pages/about", "About"),
                 ("/pages/credits", "Credits"),
-                ("/pages/goal-progress", "Goal progress"),
-                ("https://stanczakdominik.gitlab.io/vimwiki/", "Vimwiki"),
             ),
             "Pages",
         ),
@@ -1207,7 +1205,9 @@ SEARCH_FORM = """
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-# EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = r"""
+<meta http-equiv="X-Clacks-Overhead" content="GNU Terry Pratchett" />
+"""
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
